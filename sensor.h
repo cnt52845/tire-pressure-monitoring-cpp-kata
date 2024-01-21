e31a415c277ca2779b55f68a0b1afff7803cafe7
Yury Bayda <yury@bayda.pro>
2024-01-20 20:53:36 -0800

    Align with Python version

--- sensor.h
+++ sensor.h
@@ -7,10 +7,19 @@ class Sensor {
 public:
     Sensor() { srand(time(0)); }
 
-    double pop_next_pressure_psi_value()
+    double pop_next_pressure_psi_value() const
+    {
+        auto pressure_telemetry_value = sample_pressure();
+        return offset + pressure_telemetry_value;
+    }
+
+    static double sample_pressure()
     {
         // The reading of the pressure value from the sensor is simulated
-        double pressure = 16 + (float) rand() / ((float) RAND_MAX / (6));
-        return pressure;
+        double pressure_telemetry_value = (float) rand() / ((float) RAND_MAX / (6));
+        return pressure_telemetry_value;
     }
+
+private:
+    constexpr static int offset = 16;
 };
